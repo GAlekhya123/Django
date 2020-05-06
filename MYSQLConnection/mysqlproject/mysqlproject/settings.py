f69563fbcab027d5aa305e0d8852dd71d3796dd5
GAlekhya123 <alekhyaganji440@gmail.com>
2020-05-06 09:43:49 +0530

    uploaded

--- MYSQLConnection/mysqlproject/mysqlproject/settings.py
+++ MYSQLConnection/mysqlproject/mysqlproject/settings.py
@@ -130,5 +130,5 @@ EMAIL_USE_TLS=True
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
 EMAIL_HOST_USER='alekhyaparvathi123@gmail.com'
-EMAIL_HOST_PASSWORD='Parvathi@440'
+EMAIL_HOST_PASSWORD='********'
 
